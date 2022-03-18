7dd4cf16bc53699d5c15823d6a323d2a37f42d18
Dave <87390532+vadedes@users.noreply.github.com>
2022-03-19 00:38:48 +0800

    Started working on navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,8 @@
 import { About, Footer, Header, Skills, Testimonial, Work } from './container';
 import { Navbar } from './components';
 
+import './App.scss';
+
 function App() {
   return (
     <div className='app'>
--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,6 +1,23 @@
 import './Navbar.scss';
+import { images } from '../../constants';
+
+const navMenu = ['home', 'about', 'work', 'skills', 'contact'];
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <nav className='app__navbar'>
+      <div className='app__navbar-logo'>
+        <img src={images.logo} alt='logo' />
+      </div>
+      <ul className='app__navbar-links'>
+        {navMenu.map((item) => (
+          <li className='app__flex p-text' key={`link-${item}`}>
+            <div />
+            <a href={`#${item}`}>{item}</a>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
 };
 export default Navbar;
